29672961b868258f7e9f8a86876c05016be453af
Srinithareddy16 <Srinitha888@gmail.com>
2025-07-17 16:31:31 +0530

    Day 2: PostgreSQL setup + User Entity + User API

---
+++ src/main/java/com/jobportal/jobportal_backend/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.jobportal.jobportal_backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jobportal.jobportal_backend.model.User;
+import com.jobportal.jobportal_backend.repository.UserRepository;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	
+	@Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
